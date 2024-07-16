dc36a22eec9b3a9f4de6f611c4b1aed9a0b44ca3
Anuj Rawat <130895943+AnujRawat1@users.noreply.github.com>
2024-07-16 17:19:17 +0530

    Add files via upload

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    private static final double MILES_TO_CASH_RATE = 0.0035;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double milesValue = cashValue / MILES_TO_CASH_RATE ;
+        assertEquals(milesValue,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double cashValue = milesValue * MILES_TO_CASH_RATE ;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue,rewardValue.getCashValue());
     }
 }
